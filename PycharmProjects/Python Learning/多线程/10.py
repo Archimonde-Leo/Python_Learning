bea934da683af8b5da2f9128969203e4c39df0a4
Yang tianyi <15651988632@163.com>
2019-05-08 09:34:30 +0800

    多线程

--- PycharmProjects/Python Learning/多线程/10.py
+++ PycharmProjects/Python Learning/多线程/10.py
@@ -1,2 +1,36 @@
 import threading
 from time import sleep, ctime
+
+loop = [4,2]
+
+class ThreadFunc:
+
+    def __init__(self, name):
+        self.name = name
+
+    def loop(self, nloop, nsec):
+
+        print('Start loop', nloop, 'at ', ctime())
+        sleep(nsec)
+        print('End loop', nloop, 'at ', ctime())
+
+def main():
+    print("Starting at: ", ctime())
+    # ThreadFunc(“loop”).loop
+    # ==
+    # t = ThreadFunc("loop")
+    # t.loop
+    t = ThreadFunc("loop")
+    t1 = threading.Thread(target=t.loop, args=("LOOP1", 4))
+    t2 = threading.Thread(target=ThreadFunc('loop').loop, args=("LOOP2", 2))
+
+    t1.start()
+    t2.start()
+
+    t1.join()
+    t2.join()
+
+    print("All done at: ", ctime())
+
+if __name__ == '__main__':
+    main()
